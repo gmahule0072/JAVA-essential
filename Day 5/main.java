92ffc06010127f3fa6cbbd1674f53ebe25be92a1
Girish Mahule <71246951+gmahule0072@users.noreply.github.com>
2020-10-27 14:09:49 +0530

    Create main.java

---
+++ Day 5/main.java
@@ -0,0 +1,11 @@
+  
+package com.day5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.initGame();
+        game.play();
+    }
+}
